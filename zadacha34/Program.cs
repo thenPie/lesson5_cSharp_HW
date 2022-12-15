d839360852742df6ef3bff4ad3807236ff40920d
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-15 11:05:38 +0300

    Initial commit, did zadacha34

---
+++ zadacha34/Program.cs
@@ -0,0 +1,21 @@
+﻿int Rand3D() {
+    Random rd = new Random();
+    int rand = rd.Next(100, 1000);
+    return rand;
+}
+
+int[] arr = {Rand3D(), Rand3D(), Rand3D(), Rand3D()};
+int del = 0;
+
+Console.Write("[");
+for (int i = 0; i < arr.Length; i ++) {
+    Console.Write(arr[i] + (i < arr.Length - 1 ? ", " : "] -> "));
+}
+
+for(int i = 0; i < arr.Length; i++) {
+    if(arr[i] % 2 == 0) {
+        del++;
+    }
+}
+
+Console.Write(del);

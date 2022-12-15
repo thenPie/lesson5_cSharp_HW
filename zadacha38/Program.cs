b4b8c18f3f50881aff257da650bacc6414b1c724
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-15 11:24:05 +0300

    did zadacha38

---
+++ zadacha38/Program.cs
@@ -0,0 +1,26 @@
+﻿int Rand() {
+    Random rd = new Random();
+    int rand = rd.Next(1, 101);
+    return rand;
+}
+
+int[] arr = {Rand(), Rand(), Rand(), Rand()};
+int min = arr[0], max = arr[0];
+
+Console.Write("[");
+for (int i = 0; i < arr.Length; i ++) {
+    Console.Write(arr[i] + (i < arr.Length - 1 ? ", " : "] -> "));
+}
+
+for(int i = 0; i < arr.Length; i++) {
+    if(arr[i] < min) {
+        min = arr[i];
+    }
+    if(max < arr[i]) {
+        max = arr[i];
+    }
+}
+
+int difference = max - min;
+
+Console.Write(difference);

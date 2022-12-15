a6c046d1b13330e1b03347fe0a6dcb4d97240fa9
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-15 11:13:14 +0300

    did zadacha 36

---
+++ zadacha36/Program.cs
@@ -0,0 +1,21 @@
+﻿int Rand() {
+    Random rd = new Random();
+    int rand = rd.Next(-10, 21);
+    return rand;
+}
+
+int[] arr = {Rand(), Rand(), Rand(), Rand()};
+int del = 0;
+
+Console.Write("[");
+for (int i = 0; i < arr.Length; i ++) {
+    Console.Write(arr[i] + (i < arr.Length - 1 ? ", " : "] -> "));
+}
+
+for(int i = 0; i < arr.Length; i++) {
+    if(arr[i] % 2 != 0) {
+        del = del + arr[i];
+    }
+}
+
+Console.Write(del);
